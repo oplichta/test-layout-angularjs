7b248aaafa195927dbc26333cde8b6c04dca2dfa
Oskar <oskar@rescompany.com>
2020-06-09 12:47:26 +0200

    Added getMoreController. Added firebase config.

--- src/components/get-more/get-more.js
+++ src/components/get-more/get-more.js
@@ -2,5 +2,13 @@ export default (ngModule) => {
   require('./get-more.scss');
   ngModule.component('getMore', {
     template: require('./get-more.html'),
+    controller: GetMoreController,
+    controllerAs: 'GetMoreController',
   });
+
+  GetMoreController.$inject = ['$scope'];
+  function GetMoreController($scope) {
+    $scope.test = 'test';
+    console.log($scope.test);
+  }
 };
